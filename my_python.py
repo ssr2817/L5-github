9c3c47c4fe2c03b4ebb09c8767b39af27461da73
suraj_singh <surajs995@gmail.com>
2024-06-29 16:07:25 +0530

    second incremental of code

--- my_python.py
+++ my_python.py
@@ -3,4 +3,17 @@ Age = "i'm 29 "
 
 A = 10
 B = 20
-print(A+B) 
+print(A+B)
+
+# second incremental chnage
+
+X = 10
+Y = 20
+
+if X >= Y:
+    print ("X is greater")
+else:
+    print ("Y is greater")
+
+
+
